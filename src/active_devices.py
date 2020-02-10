849ed0d5cfa8f1156e4683d5dbc65e72cdf70d8c
Maya <maya@Yakovs-MacBook-Pro.local>
2020-02-09 21:14:16 -0800

    added read active devices daily

--- src/active_devices.py
+++ src/active_devices.py
@@ -1,32 +1,20 @@
+"""
+...
+"""
+
 from openeew.data.aws import AwsDataClient
-from subprocess import Popen, PIPE
-import boto3
+from subprocess import Popen
 import time
+from datetime import datetime, timedelta
+from configparser import ConfigParser
 
-# stream name : number of shards per stream
-streams = {
-    'InputReadings' :       1,
-    'OutputAccelerations':  1,
-    'OutputWarning':        1
-}
+config = ConfigParser()
+config.read_file(open('kinesis.cfg'))
+OUTPUT_ACCEL_STREAM = config.get('KINESIS', 'output_accel_stream')
+OUTPUT_WARNING_STREAM = config.get('KINESIS', 'output_warning_stream')
 
-
-def setup_streams():
-    # credentials = boto3.Session().get_credentials()
-    # connect to Kinesis
-    kinesis = boto3.client('kinesis')
-    # create streams
-    for stream_name, shards in streams.items():
-        stream = kinesis.create_stream(StreamName=stream_name, ShardCount=shards)
-    # wait until all streams have Active status
-    for stream_name in streams.keys():
-        while kinesis.describe_stream_summary(StreamName=stream_name). \
-                get('StreamDescriptionSummary').get('StreamStatus') != 'ACTIVE':
-            time.sleep(2)
-        print(f'Stream {stream_name} has been activated')
-
-
-def get_active_devices(date_utc : str):
+def get_active_devices(date_utc : str) -> list:
+    """ get active seismic devices """
     data_client = AwsDataClient('mx')
 
     devices = data_client.get_devices_as_of_date(date_utc)
@@ -37,25 +25,33 @@ def get_active_devices(date_utc : str):
 
 
 def main():
-    ## setup Kinesis streams
-    # setup_streams()
-
     # setup dockers for consumers of 2 output streams
-    stream_names = ['OutputWarning', 'OutputAccelerations']
+    stream_names = [OUTPUT_WARNING_STREAM, OUTPUT_ACCEL_STREAM]
     for stream in stream_names:
         cmd = ['docker-compose', 'run', '-e', 'STREAM_NAME={}'.format(stream), 'seismic-consumer']
         with open("/tmp/output{}.log".format(stream), "a") as output:
             process = Popen(cmd, stdout=output, stderr=output)
 
-    ## get active seismic devices
     date_utc = '2020-01-05 00:00:01'
-    active = get_active_devices(date_utc)
+    date_roll = datetime.strptime(date_utc, '%Y-%m-%d %H:%M:%S')
+    interval = timedelta(days=1)
 
-    for device in active:
-        cmd = ['docker-compose', 'run', '-e', 'DEVICE={}'.format(device), 'seismic-service']
-        with open("/tmp/output{}.log".format(device), "a") as output:
-            process = Popen(cmd, stdout=output, stderr=output)
+    active_set = set()
 
+    while date_roll < datetime.now():
+        # set up active seismic devices as stream producers in docker containers
+        active = get_active_devices(date_utc)
+        new_set = set(active)
+        to_activate = new_set - active_set
+
+        for device in to_activate:
+            cmd = ['docker-compose', 'run', '-e', 'DEVICE={}'.format(device), 'seismic-service']
+            with open("/tmp/output{}.log".format(device), "a") as output:
+                process = Popen(cmd, stdout=output, stderr=output)
+
+        date_roll = date_roll + interval
+        date_utc = date_roll.strftime('%Y-%m-%d %H:%M:%S')
+        sleep()
         #     subprocess.call("docker-compose run -e DEVICE={}  seismic-service".format(device),
         #     shell=True, stdout=output, stderr=output)
 
